64e211a92e170423bd958c698155d6a5d446fa9f
Lenny Sirivong <lsirivong@gmail.com>
2011-09-17 16:47:13 -0500

    Added quantity column to line_items table. Implemented migrate to fix duplicated line_items in carts.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,7 +42,7 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product => product)
+    @line_item = @cart.add_product(product.id)
     
     # reset the index counter
     session[:index_access_count] = 0
