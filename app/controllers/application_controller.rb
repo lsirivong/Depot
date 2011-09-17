560e6ae9d5c491035a6a744d19b333eea3bd142e
Lenny Sirivong <lsirivong@gmail.com>
2011-09-17 13:17:00 -0500

    Added cart scaffold. Added method in ApplicationController to retrieve or create carts in the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  # private makes methods only available to controllers,
+  #  never available as actions.
+  
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
