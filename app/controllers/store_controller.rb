def7a11c8b0643fec20e5afe9c1d02f52fe67c47
Lenny Sirivong <lsirivong@gmail.com>
2011-09-20 08:18:41 -0500

    Refactored cart to use partials. Changed line item controller to redirect to catalog rather than load the separate cart page. Playtime stuff from before (add delete line_item) removed.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @cart = current_cart
     
     session[:index_access_count] ||= 0
     session[:index_access_count] += 1
