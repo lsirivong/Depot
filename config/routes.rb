a679a0a78271c83e8022adc066563571074011e7
Lenny Sirivong <lsirivong@gmail.com>
2011-11-28 18:54:53 -0600

    Iteration J1: Add framework to use I18n for varying locale URLs.

--- config/routes.rb
+++ config/routes.rb
@@ -7,73 +7,80 @@ Depot::Application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
+  # scope '(:locale)' do
+  #   resources :users
+  #   resources :orders
+  #   resources :line_items
+  #   resources :carts
+  #   resources :products do
+  #     get :who_bought, :on => :member
+  #   end
+  #   root :to => 'store#index', :as => 'store'
+  # end
 
-  resources :payment_types
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
-
-  resources :products do
-    get :who_bought, :on => :member
+  scope '(:locale)' do
+    resources :users
+    resources :payment_types
+    resources :orders
+    resources :line_items
+    resources :carts
+    
+    resources :products do
+      get :who_bought, :on => :member
+    end
+    
+    # The priority is based upon order of creation:
+    # first created -> highest priority.
+    
+    # Sample of regular route:
+    #   match 'products/:id' => 'catalog#view'
+    # Keep in mind you can assign values other than :controller and :action
+    
+    # Sample of named route:
+    #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+    # This route can be invoked with purchase_url(:id => product.id)
+    
+    # Sample resource route (maps HTTP verbs to controller actions automatically):
+    #   resources :products
+    
+    # Sample resource route with options:
+    #   resources :products do
+    #     member do
+    #       get 'short'
+    #       post 'toggle'
+    #     end
+    #
+    #     collection do
+    #       get 'sold'
+    #     end
+    #   end
+    
+    # Sample resource route with sub-resources:
+    #   resources :products do
+    #     resources :comments, :sales
+    #     resource :seller
+    #   end
+    
+    # Sample resource route with more complex sub-resources
+    #   resources :products do
+    #     resources :comments
+    #     resources :sales do
+    #       get 'recent', :on => :collection
+    #     end
+    #   end
+    
+    # Sample resource route within a namespace:
+    #   namespace :admin do
+    #     # Directs /admin/products/* to Admin::ProductsController
+    #     # (app/controllers/admin/products_controller.rb)
+    #     resources :products
+    #   end
+    
+    # You can have the root of your site routed with "root"
+    # just remember to delete public/index.html.
+    # root :to => 'welcome#index'
+    root :to => 'store#index', :as => 'store'
   end
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
